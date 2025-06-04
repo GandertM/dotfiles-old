9dd17d1ea8d74c93248906b4c65da3861fa7341e
Gandert <gandert@gmail.com>
2025-06-04 16:30:43 +0200

    Adding adding tree abbrev

--- dot_config/private_fish/conf.d/eza-abbrev.fish
+++ dot_config/private_fish/conf.d/eza-abbrev.fish
@@ -2,6 +2,12 @@
 abbr -a ls 'eza --icons --group-directories-first --group --color=always'
 abbr -a ll 'eza -hl --icons --group-directories-first --group --color=always'
 
+# for ls on all entries
+abbr -a la 'eza -hl --icons --group-directories-first --group --color=always -a'
+abbr -a lac 'eza -hl --icons --group-directories-first --group --color=always -a --sort=created'
+abbr -a lam 'eza -hl --icons --group-directories-first --group --color=always -a --sort=modified'
+abbr -a las 'eza -hl --icons --group-directories-first --group --color=always -a --total-size --sort=size'
+
 # for ls on files
 abbr -a lf  'eza -hl --icons --group-directories-first --group --color=always -f'
 abbr -a lfc 'eza -hl --icons --group-directories-first --group --color=always -f --sort=created'
@@ -19,3 +25,9 @@ abbr -a lh  'eza -hl --icons --group-directories-first --group --color=always -d
 abbr -a lhc 'eza -hl --icons --group-directories-first --group --color=always -dl .* --sort=created'
 abbr -a lhm 'eza -hl --icons --group-directories-first --group --color=always -dl .* --sort=modified'
 abbr -a lhs 'eza -hl --icons --group-directories-first --group --color=always -dl .* --total-size --sort=size'
+
+# for ls on executables
+abbr -a le="find . -maxdepth 1 -type f -executable -exec eza -hl --icons --group-directories-first --group --color=always {} +"
+abbr -a les="find . -maxdepth 1 -type f -executable -exec eza -hl --icons --group-directories-first --group --color=always --sort=size {} +"
+abbr -a lem="find . -maxdepth 1 -type f -executable -exec eza -hl --icons --group-directories-first --group --color=always --sort=modified {} +"
+abbr -a lec="find . -maxdepth 1 -type f -executable -exec eza -hl --icons --group-directories-first --group --color=always --sort=created {} +"
---
+++ dot_config/private_fish/conf.d/tree-abbrev.fish
@@ -0,0 +1,17 @@
+# for tree with depth 1
+abbr -a lt="eza -lahT --icons --color=always --group-directories-first --group"
+abbr -a lts="eza -lahT --icons --color=always --group-directories-first --group --total-size --sort=size"
+abbr -a ltm="eza -lahT --icons --color=always --group-directories-first --group --sort=modified"
+abbr -a ltc="eza -lahT --icons --color=always --group-directories-first --group --sort=created"
+
+# for tree with depth 2
+abbr -a lt2="eza -lahT --icons --color=always --group-directories-first --group --level=2"
+abbr -a lts2="eza -lahT --icons --color=always --group-directories-first --group --level=2 --total-size --sort=size"
+abbr -a ltm2="eza -lahT --icons --color=always --group-directories-first --group --level=2 --sort=modified"
+abbr -a ltc2="eza -lahT --icons --color=always --group-directories-first --group --level=2 --sort=created"
+
+# for tree with depth 3
+abbr -a lt3="eza -lahT --icons --color=always --group-directories-first --group --level=3"
+abbr -a lts3="eza -lahT --icons --color=always --group-directories-first --group --level=3 --total-size --sort=size"
+abbr -a ltm3="eza -lahT --icons --color=always --group-directories-first --group --level=3 --sort=modified"
+abbr -a ltc3="eza -lahT --icons --color=always --group-directories-first --group --level=3 --sort=created"
