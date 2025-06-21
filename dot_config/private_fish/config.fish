255c21f7efa3c96f61ab9dd177f48b4af6e1093d
Gandert <gandert@gmail.com>
2025-06-21 13:53:07 +0200

    Update 5

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,7 +5,11 @@ end
 # fish greeting with fastfetch
 function fish_greeting
     fastfetch
+    echo
     echo CTRL-T - Paste the selected files and directories
+    echo CTRL-R - Paste the selected command from history
+    echo ALT-C - cd into the selected directory
+    echo
 end
 
 # fish prompt with startship
