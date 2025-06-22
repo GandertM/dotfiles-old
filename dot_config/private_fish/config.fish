4b4e1b180bdb3481253f6fd67f281c7f6e910f0d
Gandert <gandert@gmail.com>
2025-06-22 14:07:57 +0200

    Add fzf config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,14 +8,21 @@ function fish_greeting
     echo
     echo Key-bindings for fzf
     echo -------------------------------------------------
-    echo CTRL-T - Paste the selected files and directories
-    echo CTRL-R - Paste the selected command from history
+    echo CTRL-t - Paste the selected files and directories
+    echo CTRL-r - Paste the selected command from history
     echo ALT-C - cd into the selected directory
     echo -------------------------------------------------
 end
+
+# SET FZF DEFAULTS
+set FZF_DEFAULT_OPTS "--layout=reverse --border=bold --border=rounded --margin=5% --color=dark"
 
 # fish prompt with startship
 starship init fish | source
 
 # Set up fzf key bindings
+# Enables the follwoing keybindings:
+# CTRL-t - fzf select  (Paste the selected files and directories)
+# CTRL-r - fzf history (Paste the selected command from history)
+# ALT-c  - fzf cd      (cd into the selected directory)
 fzf --fish | source
