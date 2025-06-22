3008d62a82645fc84aa084ae3a83807e248c6d13
Gandert <gandert@gmail.com>
2025-06-22 14:21:17 +0200

    Update 1

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,6 @@
+# ADDING TO THE PATH
+# First line removes the path: second line sets it. Without the first line
+# your path gets massive and fish becomes very slow
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.fzf/bin $fish_user_paths
 
